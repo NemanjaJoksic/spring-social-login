85ef88fc42dba6428625c4045e563689ee1246ba
NemanjaJoksic <joksicnemanja994@gmail.com>
2023-10-09 22:55:05 +0200

    simplify SsoController

--- idms/src/main/java/org/joksin/springsociallogin/idms/controller/SsoController.java
+++ idms/src/main/java/org/joksin/springsociallogin/idms/controller/SsoController.java
@@ -1,46 +1,24 @@
 package org.joksin.springsociallogin.idms.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 @Slf4j
 @Controller
+@AllArgsConstructor
 public class SsoController {
 
     @SneakyThrows
     @GetMapping("/sso")
-    public void home(@RequestParam String redirectTo, HttpSession httpSession,
-                     HttpServletRequest request, HttpServletResponse response) {
+    public void home(@RequestParam String redirectTo, HttpSession httpSession, HttpServletResponse response) {
         log.info("Processing session ID: {}", httpSession.getId());
-
-        var cookieMap = Stream.of(request.getCookies())
-                              .collect(Collectors.toMap(Cookie::getName, Function.identity()));
-
-        if (!cookieMap.containsKey("idms-token")) {
-            var tokenCookie = new Cookie("idms-token", "TOKEN_VALUE_123");
-            tokenCookie.setSecure(true);
-            tokenCookie.setHttpOnly(true);
-
-            response.addCookie(tokenCookie);
-
-            log.info("idms-token cookie added");
-        } else {
-            log.info("idms-token cookie already exists");
-        }
-
         log.info("Redirecting to: {}", redirectTo);
-
         response.sendRedirect(redirectTo);
     }
 
--- web-app/src/main/java/org/joksin/springsociallogin/webapp/controller/HomeController.java
+++ web-app/src/main/java/org/joksin/springsociallogin/webapp/controller/HomeController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,11 +20,15 @@ public class HomeController {
 
         var cookieMap = Stream.of(request.getCookies())
                               .collect(Collectors.toMap(Cookie::getName, Function.identity()));
-
-        var idmsToken = cookieMap.get("idms-token").getValue();
         log.info("Cookie names: {}", cookieMap.keySet());
 
-        log.info("IDMS token: {}", idmsToken);
+        var idmsTokenCookie = cookieMap.get("IDMS_TOKEN");
+
+        if (Objects.nonNull(idmsTokenCookie)) {
+            log.info("IDMS token: {}", idmsTokenCookie.getValue());
+        } else {
+            log.info("Missing IDMS_TOKEN cookie");
+        }
 
         return "Web App Home Page";
     }
