15b0a67724b3c36e0ed0eff573a46037f7e704b8
Nemanja Joksic <joksicnemanja994@gmail.com>
2024-12-07 21:15:31 +0100

    add basic auth

--- idms/src/main/java/org/joksin/springsociallogin/idms/config/SecurityConfig.java
+++ idms/src/main/java/org/joksin/springsociallogin/idms/config/SecurityConfig.java
@@ -2,22 +2,40 @@ package org.joksin.springsociallogin.idms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-//                           .authorizeHttpRequests(auth -> auth.requestMatchers("/api/sessions/*").permitAll().anyRequest().authenticated())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> {
-                })
-                .build();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .oauth2Login(httpSecurityOAuth2LoginConfigurer -> {})
+        .httpBasic(basic -> {})
+        .formLogin(Customizer.withDefaults())
+        .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    return new InMemoryUserDetailsManager(
+        User.withUsername("user").password(passwordEncoder.encode("pass")).build());
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- idms/src/main/java/org/joksin/springsociallogin/idms/controller/UserController.java
+++ idms/src/main/java/org/joksin/springsociallogin/idms/controller/UserController.java
@@ -2,16 +2,29 @@ package org.joksin.springsociallogin.idms.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class UserController {
 
-    @GetMapping("/api/users/me")
-    public Object getMe(@AuthenticationPrincipal OAuth2User user) {
-        return user.getAttributes();
+  @GetMapping("/api/users/me")
+  public Object getMe(@AuthenticationPrincipal Object principal) {
+    String username;
+
+    if (principal instanceof User user) {
+      username = user.getUsername();
+    } else if (principal instanceof OAuth2User user) {
+      username = user.getName();
+    } else {
+      username = "UNKNOWN";
     }
+
+    return Map.of("username", username);
+  }
 }
